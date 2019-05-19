82c2eba6198b55844c2f1617cbc82859166af26e
Prateek Thapa <prateekthapa26@gmail.com>
2019-05-19 12:00:05 +0545

    add css for todos

--- src/Todos.svelte
+++ src/Todos.svelte
@@ -6,22 +6,56 @@
   let newTodo = "";
 </script>
 
-<form
-  on:submit={e => {
-    e.preventDefault();
-    todos = addTodo(newTodo);
-    newTodo = '';
-  }}>
-  <input type="text" bind:value={newTodo} />
-  <button type="submit">Add Todo</button>
-</form>
+<section class="todoapp">
+  <header class="header">
+    <h1>todos</h1>
 
-<ul>
-  {#each todos as todo, index}
-    <li style={todo.isCompleted ? 'text-decoration: line-through;' : ''}>
-       {todo.value}
-    </li>
-    <button on:click={() => (todos = markAsCompleted(index))}>Done</button>
-    <button on:click={() => (todos = deleteTodo(index))}>Delete</button>
-  {/each}
-</ul>
+    <form
+      on:submit={e => {
+        e.preventDefault();
+        todos = addTodo(newTodo);
+        newTodo = '';
+      }}>
+      <input
+        class="new-todo"
+        placeholder="What needs to be done?"
+        bind:value={newTodo} />
+    </form>
+  </header>
+  <section class="main">
+    <input id="toggle-all" class="toggle-all" type="checkbox" />
+    <label for="toggle-all" />
+    <ul class="todo-list">
+      {#each todos as todo, index}
+        <li>
+          <div class="view">
+            <input class="toggle" type="checkbox" />
+            <label>{todo.value}</label>
+            <button class="destroy" />
+          </div>
+          <input class="edit" value={todo.value} />
+        </li>
+      {/each}
+    </ul>
+  </section>
+  <footer class="footer">
+    <span class="todo-count">
+      <strong>{todos.length} </strong>
+      <span>{todos.length > 1 ? 'items' : 'item'}</span>
+      <span>left</span>
+    </span>
+    <ul class="filters">
+      <li>
+        <a href="#/" class="selected">All</a>
+      </li>
+      <span />
+      <li>
+        <a href="#/active" class="">Active</a>
+      </li>
+      <span />
+      <li>
+        <a href="#/completed" class="">Completed</a>
+      </li>
+    </ul>
+  </footer>
+</section>
--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
 import Todos from './Todos.svelte';
+import './main.css';
 
 export default new Todos({ target: document.body });
