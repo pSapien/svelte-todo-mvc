c6b82036af4276745b28cf60dcd547872bec7101
Prateek Thapa <prateekthapa26@gmail.com>
2019-05-25 23:36:58 +0545

    seperated into Todo svelte

---
+++ src/Todo.svelte
@@ -0,0 +1,16 @@
+<script>
+  export let todo, onDelete, onCheckoxChange;
+  let { isCompleted, value } = todo;
+</script>
+
+<li class:completed={isCompleted}>
+  <div class="view">
+    <input
+      type="checkbox"
+      bind:checked={isCompleted}
+      on:click={onCheckoxChange} />
+    <label> {value} </label>
+    <button class="destroy" on:click={onDelete} />
+  </div>
+  <input class="edit" {value} />
+</li>
