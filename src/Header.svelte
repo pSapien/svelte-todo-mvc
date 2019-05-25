4f021dab7b1b4963cc3a3b7e4b9d4591356cb650
Prateek Thapa <prateekthapa26@gmail.com>
2019-05-25 22:19:52 +0545

    made Header

---
+++ src/Header.svelte
@@ -0,0 +1,21 @@
+<script>
+  export let onSubmit,
+    newTodo = "";
+</script>
+
+<header class="header">
+  <h1>todos</h1>
+
+  <form
+    on:submit={e => {
+      e.preventDefault();
+      onSubmit(newTodo.trim());
+      newTodo = '';
+    }}>
+    <input
+      class="new-todo"
+      placeholder="What needs to be done?"
+      bind:value={newTodo} />
+  </form>
+
+</header>
