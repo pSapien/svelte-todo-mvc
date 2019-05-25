97ef5db622646c6f3834d267624a144cef4bed6b
Prateek Thapa <prateekthapa26@gmail.com>
2019-05-25 22:20:01 +0545

    made Footer component

---
+++ src/Footer.svelte
@@ -0,0 +1,29 @@
+<script>
+  export let todosLength;
+  export let updateFilter;
+
+  $: todoLeftText = todosLength > 1 ? "items" : "item";
+</script>
+
+<footer class="footer">
+  <span class="todo-count">
+    <strong>{todosLength} </strong>
+    <span>{todoLeftText}</span>
+    <span>left</span>
+  </span>
+  <ul class="filters">
+    <li>
+      <button on:click={() => updateFilter('all')} class="selected">All</button>
+    </li>
+    <span />
+    <li>
+      <button on:click={() => updateFilter('active')} class="">Active</button>
+    </li>
+    <span />
+    <li>
+      <button on:click={() => updateFilter('completed')} class="">
+        Completed
+      </button>
+    </li>
+  </ul>
+</footer>
